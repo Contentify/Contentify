2ef4fdff8e745cf95ef968cb4b82f774208176c4
siconize <siconize@gmail.com>
2014-01-28 14:23:28 +0100

    Diag module: Show some of the MySQL settings

--- app/modules/diag/controllers/AdminDiagController.php
+++ app/modules/diag/controllers/AdminDiagController.php
@@ -1,7 +1,7 @@
 <?php namespace App\Modules\Diag\Controllers;
 
 use \Carbon\Carbon as Carbon;
-use App, Config, View, BackController, File;
+use App, Config, View, BackController, File, DB, PDO;
 
 class AdminDiagController extends BackController {
 
@@ -11,24 +11,62 @@ class AdminDiagController extends BackController {
     {
         if (! $this->checkAccessRead()) return;
 
+        /*
+         * Check if Laravel is compiled to one single file
+         */
         $filename = app('path.base').'/bootstrap/compiled.php';
         if (File::exists($filename)) {
-            
             $optimized = '1 - compiled: '.Carbon::createFromTimeStamp(filemtime($filename));
         } else {
             $optimized = 0;
-        }
+        }      
 
+        /*
+         * Create array with names and values
+         */
         $settings = array(
             'Artisan optimized' => $optimized,
             'App.environment'   => App::environment(),
             'App.url'           => Config::get('app.url'),
             'App.debug'         => (int) Config::get('app.debug'),
-            'App.key'           => (Config::get('app.key') == '01234567890123456789012345678912' ? 'Dummy' : 'Updated'),
+            'App.key'           => (Config::get('app.key') == '01234567890123456789012345678912' ? 'Dummy value' : 'Valid value'),
             'Modules.mode'      => Config::get('modules::mode'),
-            'Modules.debug'     => (int) Config::get('modules::debug')
+            'Modules.debug'     => (int) Config::get('modules::debug'),
         );
+
+        /*
+         * If we use MySQL as database, add values of some MySQL variables.
+         */
+        if (Config::get('database.default') == 'mysql') {
+            $fetchMethod = Config::get('database.fetch');
+            DB::connection()->setFetchMode(PDO::FETCH_ASSOC); // We need to get the result as array of arrays 
+            $sqlVars = DB::select('SHOW VARIABLES');
+            DB::connection()->setFetchMode($fetchMethod);
+
+            $settings['MySQL.max_connections']      = $this->getSqlVar($sqlVars, 'max_connections');
+            $settings['MySQL.max_user_connections'] = $this->getSqlVar($sqlVars, 'max_user_connections');
+        }
     
         $this->pageView('diag::admin_index', compact('settings'));
+    }
+
+    /**
+     * Helper function. Returns the value of an variable from a MySQl variables array retrieved from the database.
+     * 
+     * @param  array    $vars    Array of variables
+     * @param  string   $varName Name of variable
+     * @return mixed
+     */
+    protected function getSqlVar($vars, $varName)
+    {
+        $varName = strtolower($varName);
+
+        foreach ($vars as $var) {
+            if ($var['Variable_name'] == $varName) {
+                return $var['Value'];
+            }
+        }
+
+        return null;
     }
 }
