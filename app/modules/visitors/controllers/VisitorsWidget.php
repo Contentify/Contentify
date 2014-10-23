ece53ab1962cad27568052fd904c39c96880c657
siconize <siconize@gmail.com>
2014-10-23 15:16:47 +0200

    Visitors widget bugfix

--- app/modules/visitors/controllers/VisitorsWidget.php
+++ app/modules/visitors/controllers/VisitorsWidget.php
@@ -6,13 +6,18 @@ class VisitorsWidget extends Widget {
 
     public function render($parameters = array())
     {
-        $today = DB::table('visits')->select('user_agents')
+        // Use SUM() so we will always get a result, even if there aren't any rows for today.
+        $today = DB::table('visits')->select(DB::raw('SUM(user_agents) AS user_agents'))
             ->whereVisitedAt(DB::raw('CURRENT_DATE'))
             ->remember(5)->get()[0]->user_agents;
 
-        $yesterday = DB::table('visits')->select('user_agents')
+        if (! $today) $today = 0;
+
+        $yesterday = DB::table('visits')->select(DB::raw('SUM(user_agents) AS user_agents'))
             ->whereVisitedAt(DB::raw('SUBDATE(CURRENT_DATE, 1)'))
-            ->remember(5)->get()[0]->user_agents;       
+            ->remember(5)->get()[0]->user_agents;  
+
+        if (! $yesterday) $yesterday = 0;
 
         $month = DB::table('visits')->select(DB::raw('SUM(user_agents) AS user_agents'))
             ->where(DB::raw('MONTH(visited_at)'), '=', DB::raw('MONTH(CURRENT_DATE)'))
