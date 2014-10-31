b26d15fd971adf81605d6a907da64cdf3e291355
siconize <siconize@gmail.com>
2014-10-31 15:43:40 +0100

    Video previews images now look consistent

--- app/modules/videos/views/index.blade.php
+++ app/modules/videos/views/index.blade.php
@@ -5,18 +5,19 @@
     <div class="video">
         <a href="{{{ url('videos/'.$video->id.'/'.$video->slug) }}}" data-id="{{{ $video->id }}}">
             @if ($video->provider == 'youtube')
-                <img src="http://img.youtube.com/vi/{{{ $video->permanent_id }}}/mqdefault.jpg" alt="{{{ $video->title }}}">
+            <div style="background-image: url('http://img.youtube.com/vi/{{{ $video->permanent_id }}}/mqdefault.jpg')"></div>
             @endif
 
             @if ($video->provider == 'vimeo')
+            <div></div>
             <script>
                 $.getJSON('http://vimeo.com/api/v2/video/' + {{{ $video->permanent_id }}} + '.json?callback=?', 
                 function(data) 
                 {
-                    var image = data[0].thumbnail_medium;
-                    var $el = $('.page .videos [data-id=' + {{{ $video->id }}} + ']');
-                    $el.prepend($('<img>').attr('src', image).attr('alt', '{{{ $video->title }}}'));
-                });
+                        var image = data[0].thumbnail_medium;
+                        var $el = $('.page .videos [data-id=' + {{{ $video->id }}} + '] div');
+                        $el.css('background-image', "url('" + image + "')");
+                    });
             </script>
             @endif
 
