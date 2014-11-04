662e526c028614be64c02c8802d3045c22e65a05
siconize <siconize@gmail.com>
2014-11-04 12:47:16 +0100

    Added simple web/CLI script that tests the server requirements

---
+++ tester.php
@@ -0,0 +1,85 @@
+<?php
+
+class Tester {
+
+    /**
+     * Array with paths
+     * @var array
+     */
+    protected $paths = array();
+
+    public function __construct()
+    {
+        $this->paths = require __DIR__.'/bootstrap/paths.php';
+    }
+
+    /**
+     * Run the tester
+     * @return void
+     */
+    public function run()
+    {
+        if (version_compare(PHP_VERSION, '5.4.0') >= 0) {
+            $version = 'Yes, '.phpversion();
+        } else {
+            $version = 'No, '.phpversion();
+        }
+
+        if (extension_loaded('mcrypt')) {
+            $mCrypt = 'Yes';
+        } else {
+            $mCrypt = 'No';
+        }
+
+        if (extension_loaded('fileinfo')) {
+            $fileInfo = 'Yes';
+        } else {
+            $fileInfo = 'No';
+        }
+
+        $this->line('PHP-Version: '.$version);
+        $this->line('PHP MCrypt Extension: '.$mCrypt);
+        $this->line('PHP FileInfo Extension: '.$fileInfo);
+        $this->line();
+
+        $paths = $this->paths;
+        $writableDirs = [$paths['app'].'/storage', $paths['public'].'/uploads', $paths['public'].'/rss'];
+
+        foreach ($writableDirs as $dir) {
+            if (is_writable($dir)) {
+                $this->line($dir.' is writable');
+            } else {
+                $this->line($dir.' is not writable');
+            }
+        }
+        $this->line();
+    }
+
+    /**
+     * Echo this line
+     * 
+     * @param  string $text The line of text
+     * @return void
+     */
+    protected function line($text = null)
+    {
+        if ($this->isCli()) {
+            echo $text." \n";    
+        } else {
+            echo $text.'<br>';
+        }        
+    }
+
+    /**
+     * Determine if PHP is being run from the CLI
+     * 
+     * @return boolean
+     */
+    protected function isCli()
+    {
+        return (php_sapi_name() === 'cli');
+    }
+
+}
+
+(new Tester)->run();
