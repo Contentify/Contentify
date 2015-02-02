7344c11f6df4cee582f74275e6446283c641076a
siconize <siconize@gmail.com>
2015-02-02 17:57:33 +0100

    Finished navigations module

--- app/modules/navigations/views/admin_form.blade.php
+++ app/modules/navigations/views/admin_form.blade.php
@@ -1,21 +1,26 @@
 {{ Form::errors($errors) }}
 
 @if (isset($model))
-    {{ Form::model($model, ['route' => ['admin.games.update', $model->id], 'files' => true, 'method' => 'PUT']) }}
+    {{ Form::model($model, ['route' => ['admin.navigations.update', $model->id], 'method' => 'PUT']) }}
 @else
-    {{ Form::open(['url' => 'admin/games', 'files' => true]) }}
+    {{ Form::open(['url' => 'admin/navigations']) }}
 @endif
-    {{ Form::smartText('title', trans('app.title')) }}
+    {{ Form::smartText('title', trans('app.title')) }} 
 
-    {{-- link_to('#', 'Add') --}}
-
-    <table id="items-table">
-        <tbody>
-
-        </tbody>
+    <table id="items-table" class="table">
+        <thead>
+            <tr>
+                <th>{{ trans('app.title') }}</th>
+                <th class="urls">{{ trans('app.url') }}</th>
+                <th class="actions">{{ trans('app.actions') }}</th>
+            </tr>            
+        </thead>
+        <tbody>{{-- content genereated by JS below --}}</tbody>
     </table>
 
-    {{ Form::smartTextarea('items', trans('app.items')) }}
+    {{ Form::button(HTML::fontIcon('plus-circle').' '.trans('app.create'), ['id' => 'item_add', 'class' => 'btn btn-default']) }}
+
+    {{ Form::hidden('items') }}
 
     {{ Form::actions() }}
 {{ Form::close() }}
@@ -23,6 +28,141 @@
 <script>
     $(document).ready(function()
     {
+        var $items = $('.page input[name=items]');
+        var items = JSON.parse($items.val()); // Array of objects
+        var $tableBody = $('#items-table tbody');
 
+        var template = '<div class="boxer-plain add-new" data-index="%%index%%"> {{ Form::smartText('item_title', trans('app.title')) }} {{ Form::smartText('item_url', trans('app.url')) }}</div>';
+
+        contentify.templateManager.add('itemForm', template);
+
+        function store()
+        {
+            $items.val(JSON.stringify(items));
+        }
+
+        function createUiItem(item, index)
+        {
+            var actions = '<a data-action="up" href="#">' + contentify.fontIcon('chevron-up') + '</a>' +
+                '<a data-action="down" href="#">' +  contentify.fontIcon('chevron-down') + '</a>' +
+                '<a data-action="left" href="#">' + contentify.fontIcon('chevron-left') + '</a>' +
+                '<a data-action="right" href="#">' + contentify.fontIcon('chevron-right') + '</a>' +
+                '<a data-action="edit" href="#">' + contentify.fontIcon('edit') + '</a>' + 
+                '<a data-action="delete" href="#">' + contentify.fontIcon('trash') + '</a>';
+
+            var $tr = $('<tr>').addClass('item-level-' + item.level).attr('data-index', index).append(
+                $('<td>').text(item.title)
+            ).append(
+                $('<td>').text(item.url)
+            ).append(
+                $('<td>').html(actions)
+            );
+
+            $tr.find('a').click(function(event)
+            {
+                event.preventDefault();
+
+                var action = $(this).attr('data-action');
+                var index = parseInt($(this).parent().parent().attr('data-index'));
+
+                switch (action) {
+                    case 'up':
+                        if (index > 0) {
+                            var tempItem = items[index - 1];
+                            items[index - 1] = items[index];
+                            items[index] = tempItem;
+                        }
+                        break;
+                    case 'down':
+                        if (index < items.length - 1) {
+                            var tempItem = items[index + 1];
+                            items[index + 1] = items[index];
+                            items[index] = tempItem;
+                        }
+                        break;
+                    case 'left':
+                        if (items[index].level > 0) {
+                            items[index].level--;
+                        }
+                        break;
+                    case 'right':
+                        if (items[index].level < 5) {
+                            items[index].level++;
+                        }
+                        break;
+                    case 'edit':
+                        var compiled = contentify.templateManager.get('itemForm', {index: index});
+                        var $compiled = $(compiled);
+
+                        $compiled.find('#item_title').val(items[index].title);
+                        $compiled.find('#item_url').val(items[index].url);
+
+                        $.boxer($compiled.append(
+                            $('<button>').text('{{ trans('app.save') }}').click(function()
+                            {
+                                items[index].title = $('#item_title').val();
+                                items[index].url = $('#item_url').val();
+                                renderAll();
+                                store();
+                                $.boxer('close');
+                            })
+                        ));
+
+                        break;
+                    case 'delete':
+                        items.splice(0, 1);
+                        break;
+                }
+
+                renderAll();
+                store();             
+            });
+
+            return $tr;
+        }
+
+        function renderAll()
+        {
+            $tableBody.html('');
+
+            for (var i = 0; i < items.length; i++) {
+                // Correct invalid structures, e. g. child-items without direct parents
+                if (i == 0) {
+                    if (items[i].level > 0) {
+                        items[i].level = 0;
+                    }
+                } else {
+                    if (items[i].level - items[i - 1].level > 1) {
+                        items[i].level = items[i - 1].level + 1;
+                    }
+                }
+
+                var uiItem = createUiItem(items[i], i);
+                $tableBody.append(uiItem);
+            };
+        }
+
+        $('#item_add').click(function()
+        {
+            var compiled = contentify.templateManager.get('itemForm',  {});
+            var $compiled = $(compiled);
+
+            $.boxer($compiled.append(
+                $('<button>').text('{{ trans('app.save') }}').click(function()
+                {
+                    var item = {
+                        title: $('#item_title').val(),
+                        url: $('#item_url').val(),
+                        level: 0
+                    };
+                    items.push(item);
+                    renderAll();
+                    store();
+                    $.boxer('close');
+                })
+            ));
+        });
+
+        renderAll();
     });
 </script>
