9a8ff91a72474883a9a3281f6f4ecb920dfee7b6
siconize <siconize@gmail.com>
2015-03-16 16:36:34 +0100

    Small user profile makeover

--- app/modules/users/views/show.blade.php
+++ app/modules/users/views/show.blade.php
@@ -1,130 +1,151 @@
 <h1 class="page-title">{{ $user->username }}</h1>
 
-<div class="profile-basics">
-    <ul class="list-unstyled">
-        <li>
-            <span class="title">{{ trans('app.name') }}:</span>
-            @if ($user->country->icon)
-                {{ HTML::image($user->country->uploadPath().$user->country->icon, $user->country->title) }}
-            @endif
-             {{{ $user->first_name }}} {{{ $user->last_name }}}
-        </li>
-        <li>
-            <span class="title">{{ trans('users::gender') }}:</span>
-            {{{ $user->gender }}}
-        </li>
-        <li>
-            <span class="title">{{ trans('users::language') }}:</span>
-            {{{ $user->language->title }}}
-        </li>
-        <li>
-            <span class="title">{{ trans('users::birthdate') }}:</span>
-            {{{ $user->birthdate }}}
-        </li>
-        <li>
-            <span class="title">{{ trans('users::occupation') }}:</span>
-            {{{ $user->occupation }}}
-        </li>
-        <li>
-            <span class="title">{{ trans('users::website') }}:</span>
-            @if(e($user->website))
-                {{ HTML::link(e($user->website)) }}
-            @endif
-        </li>
-        <li>
-            <span class="title">{{ trans('users::about') }}:</span>
-            {{{ $user->about }}}
-        </li>
-    </ul>
+<div class="profile-basics row">
+    <div class="col-md-8">
+        <table class="table h">
+            <tbody>
+                <tr>
+                    <th class="title">{{ trans('app.name') }}</th>
+                    <td>
+                        @if ($user->country->icon)
+                            {{ HTML::image($user->country->uploadPath().$user->country->icon, $user->country->title) }}
+                        @endif
+                        {{{ $user->first_name }}} {{{ $user->last_name }}}
+                    </td>
+                </tr>
+                <tr>
+                    <th class="title">{{ trans('users::gender') }}:</th>
+                    <td>{{{ $user->gender }}}</td>
+                </tr>
+                <tr>
+                    <th class="title">{{ trans('users::language') }}:</th>
+                    <td>{{{ $user->language->title }}}</td>
+                </tr>
+                <tr>
+                    <th class="title">{{ trans('users::birthdate') }}:</th>
+                    <td>{{{ $user->birthdate }}}</td>
+                </tr>
+                <tr>
+                    <th class="title">{{ trans('users::occupation') }}:</th>
+                    <td>{{{ $user->occupation }}}</td>
+                </tr>
+                <tr>
+                    <th class="title">{{ trans('users::website') }}:</th>
+                    <td>
+                        @if(e($user->website))
+                            {{ HTML::link(e($user->website)) }}
+                        @endif
+                    </td>
+                </tr>
+                <tr>
+                    <th class="title">{{ trans('users::about') }}:</th>
+                    <td>{{{ $user->about }}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="col-md-4">
+        @if ($user->image)
+            <img src="{{ $user->uploadPath().$user->image }}" alt="{{ $user->username }}">
+        @else
+            <img src="{{ asset('theme/user.png') }}" alt="{{ $user->username }}">
+        @endif
+    </div>
 </div>
 
 <div class="profile-socials">
-    <ul class="list-unstyled">
-        <li>
-            <span class="title">Facebook:</span>
-            {{{ $user->facebook }}}
-        </li>
-        <li>
-            <span class="title">Twitter:</span>
-            {{{ $user->twitter }}}
-        </li>
-        <li>
-            <span class="title">Skype:</span>
-            {{{ $user->skype }}}
-        </li>
-        <li>
-            <span class="title">{{ trans('users::steam_id') }}:</span>
-            <a href="http://steamcommunity.com/id/{{{ $user->steam_id }}}">{{{ $user->steam_id }}}</a>
-        </li>
-    </ul>
+    <table class="table h">
+        <tbody>
+            <tr>
+                <th class="title">Facebook:</th>
+                <td>{{{ $user->facebook }}}</td>
+            </tr>
+            <tr>
+                <th class="title">Twitter:</th>
+                <td>{{{ $user->twitter }}}</td>
+            </tr>
+            <tr>
+                <th class="title">Skype:</th>
+                <td>{{{ $user->skype }}}</td>
+            </tr>
+            <tr>
+                <th class="title">{{ trans('users::steam_id') }}:</th>
+                <td><a href="http://steamcommunity.com/id/{{{ $user->steam_id }}}">{{{ $user->steam_id }}}</a></td>
+            </tr>
+        </tbody>
+    </table>
 </div>
 
 <div class="profile-pc">
-    <ul class="list-unstyled">
-        <li>
-            <span class="title">{{ trans('users::cpu') }}:</span>
-            {{{ $user->cpu }}}
-        </li>
-        <li>
-            <span class="title">{{ trans('users::graphics') }}:</span>
-            {{{ $user->graphics }}}
-        </li>
-        <li>
-            <span class="title">{{ trans('users::ram') }}:</span>
-            {{{ $user->ram }}}
-        </li>
-        <li>
-            <span class="title">{{ trans('users::motherboard') }}:</span>
-            {{{ $user->motherboard }}}
-        </li>
-        <li>
-            <span class="title">{{ trans('users::drives') }}:</span>
-            {{{ $user->drives }}}
-        </li>
-        <li>
-            <span class="title">{{ trans('users::display') }}:</span>
-            {{{ $user->display }}}
-        </li>
-        <li>
-            <span class="title">{{ trans('users::mouse') }}:</span>
-            {{{ $user->mouse }}}
-        </li>
-        <li>
-            <span class="title">{{ trans('users::keyboard') }}:</span>
-            {{{ $user->keyboard }}}
-        </li>
-        <li>
-            <span class="title">{{ trans('users::headset') }}:</span>
-            {{{ $user->headset }}}
-        </li>
-        <li>
-            <span class="title">{{ trans('users::mousepad') }}:</span>
-            {{{ $user->mousepad }}}
-        </li>
-    </ul>
+    <table class="table h">
+        <tbody>
+            <tr>
+                <th class="title">{{ trans('users::cpu') }}:</th>
+                <td>{{{ $user->cpu }}}</td>
+            </tr>
+            <tr>
+                <th class="title">{{ trans('users::graphics') }}:</th>
+                <td>{{{ $user->graphics }}}</td>
+            </tr>
+            <tr>
+                <th class="title">{{ trans('users::ram') }}:</th>
+                <td>{{{ $user->ram }}}</td>
+            </tr>
+            <tr>
+                <th class="title">{{ trans('users::motherboard') }}:</th>
+                <td>{{{ $user->motherboard }}}</td>
+            </tr>
+            <tr>
+                <th class="title">{{ trans('users::drives') }}:</th>
+                <td>{{{ $user->drives }}}</td>
+            </tr>
+            <tr>
+                <th class="title">{{ trans('users::display') }}:</th>
+                <td>{{{ $user->display }}}</td>
+            </tr>
+            <tr>
+                <th class="title">{{ trans('users::mouse') }}:</th>
+                <td>{{{ $user->mouse }}}</td>
+            </tr>
+            <tr>
+                <th class="title">{{ trans('users::keyboard') }}:</th>
+                <td>{{{ $user->keyboard }}}</td>
+            </tr>
+            <tr>
+                <th class="title">{{ trans('users::headset') }}:</th>
+                <td>{{{ $user->headset }}}</td>
+            </tr>
+            <tr>
+                <th class="title">{{ trans('users::mousepad') }}:</span>
+                <td>{{{ $user->mousepad }}}</td>
+            </tr>
+        </tbody>
+    </table>
 </div>
 
 <div class="profile-favs">
-    <ul class="list-unstyled">
-        <li>
-            <span class="title">{{ trans('users::game') }}:</span>
-            {{{ $user->game }}}
-        </li>
-        <li>
-            <span class="title">{{ trans('users::food') }}:</span>
-            {{{ $user->food }}}
-        </li>
-        <li>
-            <span class="title">{{ trans('users::drink') }}:</span>
-            {{{ $user->drink }}}
-        </li>
-        <li>
-            <span class="title">{{ trans('users::music') }}:</span>
-            {{{ $user->music }}}
-        </li>
-        <li>
-            <span class="title">{{ trans('users::film') }}:</span>
-            {{{ $user->film }}}
-        </li>
-    </ul>
+    <table class="table h">
+        <tbody>
+            <tr>
+                <th class="title">{{ trans('users::game') }}:</th>
+                <td>{{{ $user->game }}}</td>
+            </tr>
+            <tr>
+                <th class="title">{{ trans('users::food') }}:</th>
+                <td>{{{ $user->food }}}</td>
+            </tr>
+            <tr>
+                <th class="title">{{ trans('users::drink') }}:</th>
+                <td>{{{ $user->drink }}}</td>
+            </tr>
+            <tr>
+                <th class="title">{{ trans('users::music') }}:</th>
+                <td>{{{ $user->music }}}</td>
+            </tr>
+            <tr>
+                <th class="title">{{ trans('users::film') }}:</th>
+                <td>{{{ $user->film }}}</td>
+            </tr>
+        </tbody>
+    </table>
 </div>
