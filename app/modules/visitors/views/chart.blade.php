7e23c123084b5fdd57d6afe32fde417b7fec64fb
siconize <siconize@gmail.com>
2015-05-09 21:08:19 +0200

    Bugfix: Do not force tickSize = 1 to avoid terrible results for days with many visitors

--- app/modules/visitors/views/chart.blade.php
+++ app/modules/visitors/views/chart.blade.php
@@ -8,7 +8,7 @@
             lines:  { show: true },
             points: { show: true },
             xaxis:  {min: 1, max: {{ $chart->maxDay }}, tickSize: 1, tickDecimals: 0},
-            yaxis:  {min: 0, tickSize: 1, tickDecimals: 0},
+            yaxis:  {min: 0, tickDecimals: 0},
             colors: ["#FF6600"],
             grid:   { borderWidth: 2, borderColor: '#BBB', hoverable: true, clickable: true }
         }
