c57a3fe420943915734e6caac1a2bd3f3708a3ac
siconize <siconize@gmail.com>
2015-06-15 13:08:07 +0200

    Controllers are able to return their layout view again (Laravel 4 behaviour)

--- contentify/Controllers/BaseController.php
+++ contentify/Controllers/BaseController.php
@@ -86,7 +86,17 @@ abstract class BaseController extends Controller {
     {
         $this->setupLayout();
 
-        return call_user_func_array(array($this, $method), $parameters);
+        $response = call_user_func_array(array($this, $method), $parameters);
+
+        // If no response is returned from the controller action and a layout is being
+        // used we will assume we want to just return the layout view as any nested
+        // views were probably bound on this view during this controller actions.
+        if (is_null($response) && ! is_null($this->layout))
+        {
+            $response = $this->layout;
+        }
+
+        return $response;
     }
 
     /**
