0c6ccdbb8f0993c71f7934f270f70cbb834272a9
siconize <siconize@gmail.com>
2015-08-06 12:05:50 +0200

    Do not use DB::raw for WHERE with MONTH and YEAR

--- app/Modules/Visitors/Chart.php
+++ app/Modules/Visitors/Chart.php
@@ -1,5 +1,5 @@
 <?php namespace App\Modules\Visitors;
-
+
 use Carbon, DB;
 
 class Chart {
--- app/Modules/Visitors/Http/Controllers/VisitorsWidget.php
+++ app/Modules/Visitors/Http/Controllers/VisitorsWidget.php
@@ -26,8 +26,8 @@ class VisitorsWidget extends Widget {
         $month = Cache::remember('visitors.widget.month', 30, function()
         {
             return DB::table('visits')->select(DB::raw('SUM(user_agents) AS user_agents'))
-            ->where(DB::raw('MONTH(visited_at)'), '=', DB::raw('MONTH(CURRENT_DATE)'))
-            ->where(DB::raw('YEAR(visited_at)'), '=', DB::raw('YEAR(CURRENT_DATE)'))
+            ->whereMonth('visited_at', '=', DB::raw('MONTH(CURRENT_DATE)'))
+            ->whereYear('visited_at', '=', DB::raw('YEAR(CURRENT_DATE)'))
             ->get()[0]->user_agents;
         });
 
