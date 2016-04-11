c85e4374b28969fc26128474749ec36346f168b8
siconize <siconize@gmail.com>
2016-04-11 20:21:12 +0200

    Frontend layout: Added steam login button

--- app/Modules/Auth/Resources/Views/widget_login.blade.php
+++ app/Modules/Auth/Resources/Views/widget_login.blade.php
@@ -2,4 +2,6 @@
     <a class="btn btn-default" href="{{ url('auth/login') }}">{!! HTML::fontIcon('lock') !!} {{ trans('auth::login') }}</a>
 
     <a class="btn btn-default" href="{{ url('auth/registration/create') }}">{{ trans('auth::register') }}</a>
+
+    <a class="btn btn-default" href="{{ url('auth/steam') }}" title="STEAM {{ trans('auth::login') }}">{!! HTML::fontIcon('steam') !!}</a>
 </div>
