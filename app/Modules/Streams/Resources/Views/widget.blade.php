2e9fed94b248847253f9385e7edacc80f8b0dd1d
siconize <siconize@gmail.com>
2016-05-08 16:47:30 +0200

    Streams widget: Added missing theme code

--- app/Modules/Streams/Resources/Views/widget.blade.php
+++ app/Modules/Streams/Resources/Views/widget.blade.php
@@ -1,8 +1,9 @@
 <div class="widget widget-streams">
     <ul class="list-unstyled">
         @foreach ($streams as $stream)
-            <li class="{!! $stream->online ? 'online' : 'offline' !!}">
+            <li class="{!! $stream->online ? 'online' : 'offline' !!} clearfix">
                 <a href="{!! url('streams/'.$stream->id.'/'.$stream->slug) !!}" title="{{ $stream->title }}">
+                    <div class="image" style="background-image: url('{!! $stream->thumbnail !!}')"></div>               
                     <span class="title">{{ $stream->title }}</span>
                     <span class="viewers">{!! $stream->viewers !!}</span>
                 </a>
