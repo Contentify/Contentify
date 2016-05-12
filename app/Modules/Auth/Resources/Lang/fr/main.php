a0fe2bd9054ad73caa02cf04fe43a7f2e70b45da
krysenn <camilleri.alexis@gmail.com>
2016-05-12 21:38:05 +0200

    Add new files for translation in french

---
+++ app/Modules/Auth/Resources/Lang/fr/main.php
@@ -0,0 +1,51 @@
+<?php 
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Module Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | This file keeps the language lines of the related module.
+    |
+    */
+
+    /*
+     * Logout
+     */
+    'logged_out'        => 'Déconnecté!',
+
+    /*
+     * Registration
+     */
+    'register'          => 'Inscription',
+    'registered'        => 'Inscrit avec succès!',
+    'username_taken'    => 'Nom d\'utilisateur déjà pris.',
+    'password_length'   => 'Votre mot de passe doit faire 6 caractères minimum.',
+    'register_twice'    => 'Vous êtes déjà inscrit.',
+
+    /*
+     * Restore password
+     */
+    'password_reset'    => 'Réinitialiser son mot de passe',
+    'email_gen_pw'      => 'Un email vous a été envoyé. Suivez les instructions pour changer votre mot de passe.',
+    'email_invalid'     => 'Il y a aucun utilisateur avec cette adresse email.',
+    'code_invalid'      => 'Le code ne correspondent pas.',
+    'new_pw'            => 'Nouveau mot de passe',
+    'email_new_pw'      => 'Un email contenant votre nouveau mot de passe vous a été envoyé.',
+    'pw_link'           => 'Suivez le lien afin de générer un nouveau mot de passe pour :0.',
+    'email_ignore'      => 'Si vous ne voulez pas changer votre mot de passe, ignorez cet email.',
+    'pw_generated'      => 'Un nouveau mot de passe a été généré pour :0.',
+
+    /* 
+     * Login 
+     */
+    'login'             => 'Connexion',
+
+    /*
+     * Widget
+     */
+    'backend'           => 'Admin-Panel',
+
+);
