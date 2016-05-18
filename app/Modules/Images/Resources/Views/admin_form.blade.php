61f70aebb6d283030634cb0586f32e6a69ced648
siconize <siconize@gmail.com>
2016-05-18 16:13:09 +0200

    Images module: Auto add filename as tag

--- app/Modules/Images/Resources/Views/admin_form.blade.php
+++ app/Modules/Images/Resources/Views/admin_form.blade.php
@@ -17,3 +17,22 @@
 
     {!! Form::actions() !!}
 {!! Form::close() !!}
+
+<script>
+    $(document).ready(function()
+    {
+        $('#image').change(function()
+        {
+            var files   = this.files;
+            var file    = files[0];
+            var name    = file.name;
+            var pos     = name.indexOf('.');
+
+            if (pos > 0) {
+                name = name.substr(0, pos);
+            }
+   
+            $('#tags').tagsinput('add', name);
+        });        
+    });
+</script>
