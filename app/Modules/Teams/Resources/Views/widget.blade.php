81b3a589775e308c96bed538d833c5c991f12a35
siconize <siconize@gmail.com>
2016-06-07 11:02:58 +0200

    Team widget: Link the image as well

--- app/Modules/Teams/Resources/Views/widget.blade.php
+++ app/Modules/Teams/Resources/Views/widget.blade.php
@@ -2,10 +2,13 @@
     <ul class="list-unstyled">
         @foreach ($teams as $team)
             <li>
-                @if ($team->image)
-                    <img src="{{ $team->uploadPath().$team->image }}" alt="{{ $team->title }}">
-                @endif
-                {!! link_to('teams/'.$team->id.'/'.$team->slug,  $team->title) !!}
+                <a href="{{ url('teams/'.$team->id.'/'.$team->slug) }}" title="{{ $team->title }}">
+                    @if ($team->image)
+                        <img src="{{ $team->uploadPath().$team->image }}" alt="{{ $team->title }}">
+                    @else
+                        <span class="title">{{ $team->title }}</span>
+                    @endif
+                </a>
             </li>
         @endforeach
     </ul>
