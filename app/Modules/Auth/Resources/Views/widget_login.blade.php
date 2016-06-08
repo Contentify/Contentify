bac4667e4781dfd1a4b4d6d9b491ee4785409921
siconize <siconize@gmail.com>
2016-06-08 18:50:39 +0200

    Changed the icon

--- app/Modules/Auth/Resources/Views/widget_login.blade.php
+++ app/Modules/Auth/Resources/Views/widget_login.blade.php
@@ -1,5 +1,5 @@
 <div class="widget widget-auth-login">
-    <a class="btn btn-default" href="{{ url('auth/login') }}">{!! HTML::fontIcon('lock') !!} {{ trans('auth::login') }}</a>
+    <a class="btn btn-default" href="{{ url('auth/login') }}">{!! HTML::fontIcon('unlock-alt') !!} {{ trans('auth::login') }}</a>
 
     <a class="btn btn-default" href="{{ url('auth/registration/create') }}">{{ trans('auth::register') }}</a>
 
