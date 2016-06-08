7552356dba10e34855b98be7c3e850bcdac48086
siconize <siconize@gmail.com>
2016-06-08 18:09:47 +0200

    Moved frontend.less, adjusted Gruntfile(s)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,32 @@
 //Gruntfile
 module.exports = function(grunt) {
-	require('jit-grunt')(grunt);
+    require('jit-grunt')(grunt);
 
-	grunt.initConfig({
-	    less: {
-	      	publish: {
-	        	options: {
-	          		compress: true,
-	          		yuicompress: true,
-	          		optimization: 2
-	        	},
-	        	files: {
-	        		// destination file and source file
-	          		'public/css/frontend.css': 'resources/assets/less/frontend.less', 
-	          		'public/css/backend.css': 'resources/assets/less/backend.less'
-	        	}
-	      	},
-	      	theme: {
-	        	options: {
-	          		compress: true,
-	          		yuicompress: true,
-	          		optimization: 2
-	        	},
-	        	files: {
-	        		// destination file and source file
-	          		'app/Modules/DefaultTheme/Resources/Assets/css/frontend.css': 'resources/assets/less/frontend.less', 
-	          		'app/Modules/DefaultTheme/Resources/Assets/css/backend.css': 'resources/assets/less/backend.less'
-	        	}
-	      	}
-	    },
-	    watch: {
-			styles: {
-				files: ['resources/assets/less/frontend.less', 'resources/assets/less/frontend.less'], // files to watch
-				tasks: ['less'], // tasks to run
-				options: {
-					nospawn: true
-				}
-			}
-	    }
-	});
+    grunt.initConfig({
+        less: {
+            publish: {
+                options: {
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2
+                },
+                files: {
+                    // destination file and source file
+                    'public/css/backend.css': 'resources/assets/less/backend.less'
+                }
+            }
+        },
+        watch: {
+            styles: {
+                files: ['resources/assets/less/backend.less'], // files to watch
+                tasks: ['less'], // tasks to run
+                options: {
+                    nospawn: true
+                }
+            }
+        }
+    });
 
-	grunt.registerTask('default', ['less:publish']); // compile the less files of the cms to the public css css folder
-	grunt.registerTask('theme', ['less:theme']); // compile the less files of the cms to the default theme css folder
-	grunt.registerTask('watch', ['less:publish', 'watch']);	
+    grunt.registerTask('default', ['less:publish']); // compile the less files of the cms to the public css folder
+    grunt.registerTask('watch', ['less:publish', 'watch']); 
 };
