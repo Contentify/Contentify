a2b7eeffd1ebd8735836fd1b4b499d7e41ed9ef2
siconize <siconize@gmail.com>
2016-09-17 16:22:20 +0200

    Cups: Added French and German translations

---
+++ app/Modules/Cups/Resources/Lang/de/main.php
@@ -0,0 +1,43 @@
+<?php 
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Module Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | This file keeps the language lines of the related module.
+    |
+    */
+    
+    // Cup
+    'join_at'           => 'Join um',
+    'check_in_at'       => 'Check-In um',
+    'start_at'          => 'Start um',
+    'checked_in'        => 'Eingecheckt',
+    'seeding'           => 'Seeding',
+    'players_per_team'  => 'Spieler pro Team',
+    'bracket'           => 'Turnierbaum',
+    'cup_full'          => 'Die maximale Teilnehmeranzahl wurde erreicht.',
+    'referees'          => 'Schiedsrichter',
+    'swap'              => 'Tauschen',
+
+    // Team
+    'not_organizer'     => 'Nur ein Teammitglied mit Organisator-Rechten darf diese Aktion durchführen.',
+    'organizer'         => 'Organisator',
+    'user_conflict'     => 'Ein User kann sich nicht in mehr als einem Team pro Cup befinden.',
+    'team_locked'       => 'Das Team ist gesperrt, solange es an einem Cup teilnimmt. Es können keine Veränderungen vorgenommen werden.',
+    'edit_team'         => 'Team editieren',
+    'team_deleted'      => 'Dieses Team wurde gelöscht.',
+    'my_teams'          => 'Meine Teams',
+    'wrong_password'    => 'Ungültiges Passwort! Bitte noch einmal versuchen.',
+    'min_organizers'    => 'Aktion kann nicht durchgeführt werden. Das Team muss über mindestens einen Organisator verfügen.',
+
+    // Match
+    'confirm_score'     => 'Ergebnis bestätigen',
+    'new_match'         => 'Ein neues Match wurde erstellt!',
+    'change_winner'     => 'Gewinner ändern',
+    'next_match'        => 'Nächstes Match',
+
+);
---
+++ app/Modules/Cups/Resources/Lang/fr/main.php
@@ -0,0 +1,43 @@
+<?php 
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Module Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | This file keeps the language lines of the related module.
+    |
+    */
+    
+    // Cup
+    'join_at'           => 'Ouverture le',
+    'check_in_at'       => 'Check-in le',
+    'start_at'          => 'Début le',
+    'checked_in'        => 'Confirmé',
+    'seeding'           => 'Seeding',
+    'players_per_team'  => 'Joueurs par équipe',
+    'bracket'           => 'Arbre de tournoi',
+    'cup_full'          => 'Le nombre maximum de participants est atteint.',
+    'referees'          => 'Arbitres',
+    'swap'              => 'Permuter',
+
+    // Team
+    'not_organizer'     => 'C\'est uniquement le capitaine qui peut faire cette action.',
+    'organizer'         => 'Capitaine',
+    'user_conflict'     => 'A user cannot be in more than one team per cup.',
+    'team_locked'       => 'L\'équipe est verrouillée car elle participe à un tournoi. Aucun changement n\'est accepté.',
+    'edit_team'         => 'Modifier l\'équipe',
+    'team_deleted'      => 'L\'équipe a été supprimée.',
+    'my_teams'          => 'Mes équipes',
+    'wrong_password'    => 'Mauvais mot de passe! Réessayer.',
+    'min_organizers'    => 'Ce n\'est pas possible. L\'équipe doit avoir au moins un capitaine.',
+
+    // Match
+    'confirm_score'     => 'Confirmer le score',
+    'new_match'         => 'Un nouveau match a été généré!',
+    'change_winner'     => 'Changer de gagnant',
+    'next_match'        => 'Match Suivant',
+
+);
