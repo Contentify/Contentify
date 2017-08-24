457383dd9853703b524847f0968301f5e109346f
Christoph K <siconize@gmail.com>
2017-08-24 21:05:00 +0200

    Bugfix: Reverted the change of the controller namespace

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -14,7 +14,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    protected $namespace = 'App\Http\Controllers';
+    protected $namespace = null;
 
     /**
      * Define your route model bindings, pattern filters, etc.
