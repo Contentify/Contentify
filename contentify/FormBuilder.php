9312961757fbc909c4c95a656ae6a1471679471d
ChrisKonnertz <siconize@gmail.com>
2017-08-27 12:21:15 +0200

    Bugfix form MySQL having a problem with an empty string instead of 0 for a boolean

--- contentify/FormBuilder.php
+++ contentify/FormBuilder.php
@@ -279,8 +279,11 @@ class FormBuilder extends OriginalFormBuilder {
     {
         $value = self::getDefaultValue($name, $default);
 
-        // Bugfix for Laravel checkbox bug ( http://nielson.io/2014/02/handling-checkbox-input-in-laravel/ ):
-        $checkbox = self::hidden($name, false).self::checkbox($name, 1, $value);
+        // We add a hidden field as a bugfix for a Laravel checkbox bug
+        // ( http://nielson.io/2014/02/handling-checkbox-input-in-laravel/ )
+        // We set the value to 0 instead of false, because false will be
+        // transformed to an empty string and MySQL cannot handle that
+        $checkbox = self::hidden($name, 0).self::checkbox($name, 1, $value);
         $partial = self::smartGroupOpen($name, $title)
             .'<div class="checkbox">'.$checkbox.'</div>'
             .self::smartGroupClose();
