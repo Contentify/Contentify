7ef30de4220f7a89732b972723157a70c0223cf8
ChrisKonnertz <siconize@gmail.com>
2017-09-27 14:06:01 +0200

    Removed unnecessary spaces

--- app/Modules/Dashboard/Resources/Views/admin_index.blade.php
+++ app/Modules/Dashboard/Resources/Views/admin_index.blade.php
@@ -5,41 +5,41 @@
         <a href="{!! url('admin/news') !!}">{!! HTML::fontIcon('newspaper-o') !!} {{ trans('app.object_news') }}</a>
     </div>
     <div>
-        <a href="{!! url('admin/pages') !!}">{!! HTML::fontIcon('file') !!}  {{ trans('app.object_pages') }}</a>
+        <a href="{!! url('admin/pages') !!}">{!! HTML::fontIcon('file') !!} {{ trans('app.object_pages') }}</a>
     </div>
     <div>
-        <a href="{!! url('admin/matches') !!}">{!! HTML::fontIcon('crosshairs') !!}  {{ trans('app.object_matches') }}</a>
+        <a href="{!! url('admin/matches') !!}">{!! HTML::fontIcon('crosshairs') !!} {{ trans('app.object_matches') }}</a>
     </div>
     <div>
-        <a href="{!! url('admin/images') !!}">{!! HTML::fontIcon('image') !!}  {{ trans('app.object_images') }}</a>
+        <a href="{!! url('admin/images') !!}">{!! HTML::fontIcon('image') !!} {{ trans('app.object_images') }}</a>
     </div>
     <div>
-        <a href="{!! url('admin/videos') !!}">{!! HTML::fontIcon('youtube-play') !!}  {{ trans('app.object_videos') }}</a>
+        <a href="{!! url('admin/videos') !!}">{!! HTML::fontIcon('youtube-play') !!} {{ trans('app.object_videos') }}</a>
     </div>
     <div>
-        <a href="{!! url('admin/downloads') !!}">{!! HTML::fontIcon('folder') !!}  {{ trans('app.object_downloads') }}</a>
+        <a href="{!! url('admin/downloads') !!}">{!! HTML::fontIcon('folder') !!} {{ trans('app.object_downloads') }}</a>
     </div>
 
     <!-- Second row  -->
 
     <div>
-        <a href="{!! url('admin/users') !!}">{!! HTML::fontIcon('user') !!}  {{ trans('app.object_users') }}</a>
+        <a href="{!! url('admin/users') !!}">{!! HTML::fontIcon('user') !!} {{ trans('app.object_users') }}</a>
     </div>
 
     <div>
-        <a href="{!! url('admin/members') !!}">{!! HTML::fontIcon('group') !!}  {{ trans('app.object_members') }}</a>
+        <a href="{!! url('admin/members') !!}">{!! HTML::fontIcon('group') !!} {{ trans('app.object_members') }}</a>
     </div>
     <div>
-        <a href="{!! url('admin/teams') !!}">{!! HTML::fontIcon('flag') !!}  {{ trans('app.object_teams') }}</a>
+        <a href="{!! url('admin/teams') !!}">{!! HTML::fontIcon('flag') !!} {{ trans('app.object_teams') }}</a>
     </div>
     <div>
-        <a href="{!! url('admin/contact') !!}">{!! HTML::fontIcon('envelope') !!}  {{ trans('app.object_messages') }}</a>
+        <a href="{!! url('admin/contact') !!}">{!! HTML::fontIcon('envelope') !!} {{ trans('app.object_messages') }}</a>
     </div>
     <div>
-        <a href="{!! url('admin/visitors') !!}">{!! HTML::fontIcon('pie-chart') !!}  {{ trans('app.object_visitors') }}</a>
+        <a href="{!! url('admin/visitors') !!}">{!! HTML::fontIcon('pie-chart') !!} {{ trans('app.object_visitors') }}</a>
     </div>
     <div>
-        <a href="{!! url('admin/config') !!}">{!! HTML::fontIcon('cog') !!}  {{ trans('app.object_config') }}</a>
+        <a href="{!! url('admin/config') !!}">{!! HTML::fontIcon('cog') !!} {{ trans('app.object_config') }}</a>
     </div>
 </div>
 
