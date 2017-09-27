adbfc5cb8122c7de3caea715ad2e07bbdb27f3dc
ChrisKonnertz <siconize@gmail.com>
2017-09-27 13:48:18 +0200

    We do not need to check this twice

--- contentify/Middleware/HttpsRedirect.php
+++ contentify/Middleware/HttpsRedirect.php
@@ -18,12 +18,10 @@ class HttpsRedirect {
         // Redirect to HTTPS version of the current URL if the protocol is not HTTPS but HTTPS support is enabled
         if (! $request->secure() and $apiKey) {
             if (! $request->ajax()) { // Ignore AJAX requests / allow them to use HTTP if they want to
-                if (! $request->secure()) {
-                    if ($request->path() === '/') { // Only when on landing page
-                        $newUrl = url('/', [], true);
+                if ($request->path() === '/') { // Only when on landing page
+                    $newUrl = url('/', [], true);
 
-                        return redirect($newUrl, 301); // 301 = Moved Permanently
-                    }
+                    return redirect($newUrl, 301); // 301 = Moved Permanently
                 }
             }
         }
