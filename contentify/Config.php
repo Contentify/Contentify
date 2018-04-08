421971824c33f5b450f641fa3537b3a1322f502f
ChrisKonnertz <siconize@gmail.com>
2018-04-08 12:48:51 +0200

    Added a lot of comments to the config class

--- contentify/Config.php
+++ contentify/Config.php
@@ -7,18 +7,25 @@ use DateTime;
 use DB;
 use Illuminate\Support\Facades\Config as LaravelConfig;
 
+/**
+ * Classical Laravel config files in the "config" folder are awesome.
+ * However, they miss one important feature: They are read-only.
+ * This class removes this constraint by using the database to
+ * persist config values. Therefore it introduces its store() method.
+ * It uses (file) caching to reduce database queries to a minimum.
+ */
 class Config extends LaravelConfig
 {
 
     /**
-     * Cache key prefix: The corresponding values contain booleans that are true if the
-     * config key has been retrieved from the DB already
+     * Cache key prefix: The corresponding values contain booleans that are true
+     * if the config key has been retrieved from the DB already
      */
     const CACHE_IN_DB_PREFIX = 'config::inDb.';
 
     /**
-     * Cache key prefix: The corresponding values were cached after they have been
-     * retrieved from the DB
+     * Cache key prefix: The corresponding config values were cached after
+     * they have been retrieved from the DB
      */
     const CACHE_VALUES_PREFIX = 'config::values.';
 
@@ -46,13 +53,18 @@ class Config extends LaravelConfig
                     return true;
                 }
             } else {
+                // Try to retrieve the config value from the DB. The result will be a \stdClass or null
                 $result = DB::table('config')->whereName($key)->first();
 
                 if (is_null($result)) {
+                    // Remember that this value is NOT stored in the DB - so we do not have to try to query it
                     Cache::put(self::CACHE_IN_DB_PREFIX.$key, false, self::CACHE_TIME);
                 } else {
                     Cache::put(self::CACHE_IN_DB_PREFIX.$key, true, self::CACHE_TIME);
+
+                    // Cache the config value
                     Cache::put(self::CACHE_VALUES_PREFIX.$key, $result->value, self::CACHE_TIME);
+
                     return true;                    
                 }
             }
@@ -82,16 +94,22 @@ class Config extends LaravelConfig
                 $inDb = Cache::get(self::CACHE_IN_DB_PREFIX.$key);
 
                 if ($inDb) {
+                    // Return the cached config value
                     return Cache::get(self::CACHE_VALUES_PREFIX.$key);
                 }
             } else {
+                // Try to retrieve the config value from the DB. The result will be a \stdClass or null
                 $result = DB::table('config')->whereName($key)->first();
 
                 if (is_null($result)) {
+                    // Remember that this value is NOT stored in the DB - so we do not have to try to query it
                     Cache::put(self::CACHE_IN_DB_PREFIX.$key, false, self::CACHE_TIME);
                 } else {
                     Cache::put(self::CACHE_IN_DB_PREFIX.$key, true, self::CACHE_TIME);
+
+                    // Cache the config value
                     Cache::put(self::CACHE_VALUES_PREFIX.$key, $result->value, self::CACHE_TIME);
+
                     return $result->value;                    
                 }
             }
@@ -105,15 +123,17 @@ class Config extends LaravelConfig
     }
 
     /**
-     * Store a given configuration value into DB.
+     * Store a given configuration value into the "config" table of the DB.
+     * If the value does not exist a new record will be created.
      *
-     * @param string $key     The name of the key
-     * @param mixed  $value   The value
+     * @param string $key   The name of the key
+     * @param mixed  $value The value - will be stored as a string
      * @return void
      */
     public static function store($key, $value)
     {
-        $result = DB::table('config')->whereName($key)
+        $result = DB::table('config')
+            ->whereName($key)
             ->update(array('value' => $value, 'updated_at' => new DateTime()));
 
         /*
@@ -145,6 +165,7 @@ class Config extends LaravelConfig
 
     /**
      * Clear the cache for a given configuration key.
+     * It does not matter if this key exists in the cache.
      *
      * @param string $key The name of the key
      * @return void
