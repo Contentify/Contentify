06fcfd6ed69820e7e3e8ba178065f6eb9b16b9c6
Christoph K <siconize@gmail.com>
2018-04-19 20:21:15 +0200

    Added user criteria and avoid exception

--- app/Modules/Users/Http/Controllers/RandomUserWidget.php
+++ app/Modules/Users/Http/Controllers/RandomUserWidget.php
@@ -19,7 +19,12 @@ class RandomUserWidget extends Widget
         $view = Cache::get($key);
 
         if ($view === null) {
-            $user = User::orderBy(DB::raw('RAND()'))->first();
+            $user = User::whereBanned(false)->whereNotNull('last_active')->orderBy(DB::raw('RAND()'))->first();
+
+            // If there is not user matching the criteria, show nothing at all
+            if (! $user) {
+                return '';
+            }
 
             $view = View::make('users::widget_random_user', compact('user'))->render();
 
