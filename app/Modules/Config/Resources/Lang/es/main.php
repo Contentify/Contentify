e2d7f36dacb340f0ccd8f492a819bc3d343a2d17
Oriol Navascuez <onavascuez@gmail.com>
2018-09-06 20:07:52 +0200

    Languages ES in Modules

---
+++ app/Modules/Config/Resources/Lang/es/main.php
@@ -0,0 +1,37 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Module Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | This file keeps the language lines of the related module.
+    |
+    */
+
+    'permissions'         => 'Permisos',
+
+    'button_info'         => 'PHP-Info',
+    'button_optimize'     => 'Optimizar DB',
+    'button_dump'         => 'Exportar DB',
+    'button_log'          => 'Log',
+    'button_compile_less' => 'Compilar LESS',
+    'button_clear_cache'  => 'Limpiar Cache',
+
+    'db_export'           => 'La base de datos se ha exportado.',
+    'db_file'             => 'Un archivo .sql se ha creado en:<br>:0',
+    'not_supported'       => 'Lo sentimos, :0 esta característica no está soportada.',
+
+    'log_empty'           => 'El archivo de log está vacío.',
+
+    /*
+     * Config
+     */
+    'website_name'        => 'Nombre de la web',
+    'analytics'           => 'Código de Analytics',
+    'registration'        => 'Registro desbloqueado',
+    'db_backup'           => 'Cópia de la Base de Datos automática',
+
+);
---
+++ app/Modules/Contact/Resources/Lang/es/main.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Module Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | This file keeps the language lines of the related module.
+    |
+    */
+
+    'message_sent'  => '¡Gracias! Tu mensaje ha sido enviado.',
+    'application'   => 'Solicitud', // If the user applies for a role in one of the teams
+
+    /*
+     * Email
+     */
+    'mail_title'    => 'Nuevo mensaje de contacto',
+    'mail_created'  => 'creado un nuevo mensaje de contacto.',
+    'mail_link'     => 'Haz click aquí para ver el mensaje.',
+
+);
