c228e9b439b7dc8feccbef8c91836131eca282e7
Oriol Navascuez <onavascuez@gmail.com>
2018-09-07 11:35:47 +0200

    Create main.php

---
+++ app/Modules/News/Resources/Lang/es/main.php
@@ -0,0 +1,20 @@
+<?php 
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Module Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | This file keeps the language lines of the related module.
+    |
+    */
+   
+    'summary'       => 'Resumen',
+    'written_by'    => 'escrito por',
+    'in'            => 'en', // in category
+    'publish_at'    => 'Publicado en', // in category
+    'rss_last'      => 'Últimas 20 notícias',
+
+);
