eec4986c53b28d251b3082b2703c19019cd6371d
Oriol Navascuez <onavascuez@gmail.com>
2018-09-07 12:29:06 +0200

    Create main.php

---
+++ app/Modules/Users/Resources/Lang/es/main.php
@@ -0,0 +1,96 @@
+<?php 
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Module Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | This file keeps the language lines of the related module.
+    |
+    */
+   
+    /*
+     * Profile
+     */
+    'first_name'    => 'Nombre',
+    'last_name'     => 'Apellido',
+    'gender'        => 'Género',
+    'localisation'  => 'Localización',
+    'local_info'    => 'Setea el idioma del interfaz y el formato de fecha.',
+    'birthdate'     => 'Fecha de nacimiento',
+    'occupation'    => 'Ocupación',
+
+    'about'         => 'Sobre tí',
+    'signature'     => 'Firma',
+
+    'steam_id'      => 'Steam ID',
+
+    'cpu'           => 'CPU',
+    'graphics'      => 'Tarjeta gráfica',
+    'ram'           => 'RAM',
+    'motherboard'   => 'Placa base',
+    'drives'        => 'Drives',
+    'display'       => 'Pantalla',
+    'mouse'         => 'Ratón',
+    'keyboard'      => 'Teclado',
+    'headset'       => 'Auriculares',
+    'mousepad'      => 'Alfombrilla',
+
+    'food'          => 'Comida',
+    'drink'         => 'Bebida',
+    'music'         => 'Música',
+    'film'          => 'Película',
+
+    'image'         => 'Foto',
+    'avatar'        => 'Avatar',
+
+    'send_msg'      => 'Enviar mensaje',
+    'add_friend'    => 'Añadir como amigo',
+
+    /*
+     * Gender
+     */
+    'unknown'       => 'Desconocido',
+    'female'        => 'Mujer',
+    'male'          => 'Hombre',
+    'other'         => 'Otro',
+
+    'change'        => 'Cambiar...',
+
+    /*
+     * Change password
+     */
+    'change_pw'     => 'Cambiar la contraseña',
+    'current_pw'    => 'Contraseña actual',
+    'new_pw'        => 'Nueva contraseña',
+
+    /*
+     * User list
+     */
+    'last_login'    => 'Último inicio de sesión',
+
+    /*
+     * Backend
+     */
+    'membership'    => 'Calidad de miembro',
+    'banned'        => 'Baneado',
+    'activated'     => 'Activado',
+
+    'action_activated'     => 'El usuario ha sido activado.',
+    'action_deactivated'   => 'El usuario ha sido desactivado.',
+
+    /* 
+     * Widget
+     */
+    'latest_users'  => 'Últimos usuarios',
+
+    /*
+     * Activities
+     */
+    'frontend'      => 'Frontal', 
+    'model_class'   => 'Modelo',
+    'activity'      => 'Actividad',
+
+);
