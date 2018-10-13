1d0290c693cc1236776abc10ba79c6dbb6016b34
Chris Konnertz <siconize@gmail.com>
2018-10-13 19:56:26 +0200

    Use position attribute for ordering

--- app/Modules/Teams/Http/Controllers/TeamsWidget.php
+++ app/Modules/Teams/Http/Controllers/TeamsWidget.php
@@ -12,9 +12,9 @@ class TeamsWidget extends Widget
     public function render(array $parameters = array())
     {
         if (isset($parameters['categoryId'])) {
-            $teams = Team::whereTeamCatId($parameters['categoryId'])->published()->orderBy('title', 'ASC')->get();
+            $teams = Team::whereTeamCatId($parameters['categoryId'])->published()->orderBy('position', 'ASC')->get();
         } else {
-            $teams = Team::published()->orderBy('title', 'ASC')->get();
+            $teams = Team::published()->orderBy('position', 'ASC')->get();
         }
 
         return View::make('teams::widget', compact('teams'))->render();
