dc79b0795ce36a2dbf13644f10f9accefeaf71bf
Chris <siconize@gmail.com>
2018-12-10 19:29:57 +0100

    Bugfix: Do no show interal polls in widget

--- app/Modules/Polls/Http/Controllers/PollsWidget.php
+++ app/Modules/Polls/Http/Controllers/PollsWidget.php
@@ -11,7 +11,13 @@ class PollsWidget extends Widget
 
     public function render(array $parameters = array())
     {
-        $poll = Poll::orderBy('open', 'desc')->orderBy('created_at', 'desc')->first();
+        $hasAccess = (user() and user()->hasAccess('internal'));
+
+        $query = Poll::orderBy('open', 'desc')->orderBy('created_at', 'desc');
+        if (! $hasAccess) {
+            $query->whereInternal(false);
+        }
+        $poll = $query->first();
 
         $userVoted = (user() and $poll) ? $poll->userVoted(user()) : false;
 
