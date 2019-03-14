4a70f51cca31994538e17e8986e0f863117ee980
Chris Konnertz <siconize@gmail.com>
2019-03-14 13:30:52 +0100

    Added link to documentation

--- contentify/ModuleRoute.php
+++ contentify/ModuleRoute.php
@@ -9,6 +9,7 @@ use Route;
  * This class decorates the original Router class of Laravel.
  * It adds support for module routes so that defining module routes becomes easier.
  *
+ * @see https://github.com/Contentify/Contentify/wiki/Module-Routing
  * @see \Illuminate\Routing\Router
  */
 class ModuleRoute
