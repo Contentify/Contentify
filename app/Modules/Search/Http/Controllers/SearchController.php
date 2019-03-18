0fd351c0820374517c574626f2c38bcdbaacd812
Chris Konnertz <siconize@gmail.com>
2019-03-18 14:05:53 +0100

    Refactoring: Extract search method

--- app/Modules/Search/Http/Controllers/SearchController.php
+++ app/Modules/Search/Http/Controllers/SearchController.php
@@ -28,43 +28,55 @@ class SearchController extends FrontController
         $validator = Validator::make(['subject' => $subject], ['subject' => 'required|min:3']);
         
         if ($validator->passes()) {
-            /** @var \Caffeinated\Modules\Contracts\Repository $moduleRepo */
-            $moduleRepo = app()['modules'];
-            $modules = $moduleRepo->all(); // Retrieve all module info objects
-
-            $resultBags = array();
-            foreach ($modules as $module) {
-                if (! $module['enabled']) continue;
-                
-                $controllerNames = isset($module['search']) ? $module['search'] : null;
-                if ($controllerNames) {
-                    // A module might have more than one controller that supports the search:
-                    foreach ($controllerNames as $controllerName) {
-                        $classPath = 'App\Modules\\'.ucfirst($module['slug'])
-                            .'\Http\Controllers\\'.ucfirst($controllerName).'Controller';
-
-                        $controller = new $classPath; // Create an instance of the controller...
-
-                        if (! $controller instanceof GlobalSearchInterface) {
-                            throw new Exception(
-                                'Error: Controller "'.$classPath.'" does not implement the GlobalSearchInterface. '.
-                                'You have to implement this interface if you want this controller to be searchable.'
-                            );
-                        }
-
-                        $results = $controller->globalSearch($subject); // ...and call the search method.
-                        
-                        if (is_array($results) and sizeof($results) > 0) {
-                            $resultBags[] = ['title' => $controllerName, 'results' => $results];
-                        }
-                    }
-                }
-            }
-
+            $resultBags = $this->performSearch($subject);
+            
             Input::flash(); // We keep the subject and display it in the form
             $this->pageView('search::form', compact('resultBags'));
         } else {
             return Redirect::to('search')->withInput()->withErrors($validator->messages());
         }
+    }
+    
+    /**
+     * Performs a global search in the frontend. Returns the results as an array of arrays.
+     *
+     * @param string $subject
+     * @return array
+     */
+    public function performSearch(string $subject) : array
+    {
+        /** @var \Caffeinated\Modules\Contracts\Repository $moduleRepo */
+        $moduleRepo = app()['modules'];
+        $modules = $moduleRepo->all(); // Retrieve all module info objects
+
+        $resultBags = array();
+        foreach ($modules as $module) {
+            if (! $module['enabled']) continue;
+
+            $controllerNames = isset($module['search']) ? $module['search'] : null;
+            if ($controllerNames) {
+                // A module might have more than one controller that supports the search:
+                foreach ($controllerNames as $controllerName) {
+                    $classPath = 'App\Modules\\'.ucfirst($module['slug'])
+                        .'\Http\Controllers\\'.ucfirst($controllerName).'Controller';
+
+                    $controller = new $classPath; // Create an instance of the controller...
+
+                    if (! $controller instanceof GlobalSearchInterface) {
+                        throw new Exception(
+                            'Error: Controller "'.$classPath.'" does not implement the GlobalSearchInterface. '.
+                            'You have to implement this interface if you want this controller to be searchable.'
+                        );
+                    }
+
+                    $results = $controller->globalSearch($subject); // ...and call the search method.
+
+                    if (is_array($results) and sizeof($results) > 0) {
+                        $resultBags[] = ['title' => $controllerName, 'results' => $results];
+                    }
+                }
+            }
+        }
+
     }
 }
