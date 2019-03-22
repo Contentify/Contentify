0a45ff6e917d56b870a9c09be033ec43a7dcd8dc
Chris Konnertz <siconize@gmail.com>
2019-03-22 13:30:51 +0100

    Extracted "delete model files" method from ModelHandler

--- contentify/ModelHandler.php
+++ contentify/ModelHandler.php
@@ -557,40 +557,8 @@ class ModelHandler
             }
         }
 
-        /*
-         * Delete related files even if it's only a soft deletion.
-         */
-        if ((! method_exists($modelClass, 'trashed') or ! $model->trashed()) 
-            and isset($modelClass::$fileHandling) and sizeof($modelClass::$fileHandling) > 0) {
-            
-            $filePath = $model->uploadPath(true);
-
-            foreach ($modelClass::$fileHandling as $fieldName => $fieldInfo) {
-                if (! is_array($fieldInfo)) {
-                    $fieldName = $fieldInfo;
-                    $fieldInfo = ['type' => 'file'];
-                }
-
-                File::delete($filePath.$model->$fieldName);
-
-                /*
-                 * Delete image thumbnails
-                 */
-                if (strtolower($fieldInfo['type']) == 'image' and isset($fieldInfo['thumbnails'])) {
-                    $thumbnails = $fieldInfo['thumbnails'];
-                    if (! is_array($thumbnails)) {
-                        $thumbnails = compact('thumbnails'); // Ensure $thumbnails is an array
-                    }
-
-                    foreach ($thumbnails as $thumbnail) {
-                        $filename = $filePath.$thumbnail.'/'.$model->$fieldName;
-                        if (File::isFile($filename)) {
-                            File::delete($filename);
-                        }
-                    }
-                }
-            }
-        }
+        // Delete related files even if it's only a soft deletion.
+        $this->uploader-deleteModelFiles($model);
 
         if (! method_exists($modelClass, 'trashed') or ! $model->trashed()) {
             $modelClass::destroy($id); // Delete model. If soft deletion is enabled for this model it's a soft deletion
