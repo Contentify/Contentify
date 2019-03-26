2d9d0c3b06f0b63993849cc0c0831a2081e6bc7a
Chris Konnertz <siconize@gmail.com>
2019-03-26 14:01:00 +0100

    Extract cup seeding code to cup class

--- app/Modules/Cups/Http/Controllers/AdminCupsController.php
+++ app/Modules/Cups/Http/Controllers/AdminCupsController.php
@@ -100,69 +100,7 @@ class AdminCupsController extends BackController
         /** @var Cup $cup */
         $cup = Cup::findOrFail($cupId);
 
-        // Get all participants that are checked-in and randomize their order
-        $participants = $cup->participants()->where(DB::raw('`cups_participants`.`checked_in`'), true)
-            ->get()->shuffle();
-
-        if ($participants->isEmpty()) {
-            $this->alertFlash(trans('app.not_possible'));
-            return Redirect::to('admin/cups');
-        }
-
-        // Set the amount of slots to the lowest possible value
-        // (not less than the number of participants, of course)
-        $slotValues = Cup::$slotValues;
-        arsort($slotValues); // Sort from high to low
-        foreach ($slotValues as $slots) {
-            if ($slots < sizeof($participants)) {
-                $cup->save();
-                break;
-            }
-            $cup->slots = $slots;
-        }
-
-        $matches = Match::whereCupId($cupId)->where('winner_id', '>', 0)->where('right_participant_id', '!=', 0)->get();
-
-        // (Re-)Seeding is not possible once matches have been played
-        if (sizeof($matches) > 0) {
-            $this->alertFlash(trans('app.not_possible'));
-            return Redirect::to('admin/cups');
-        }
-
-        // Delete the existing matches
-        Match::whereCupId($cupId)->delete();
-
-        // The number of matches that we will generate is ALWAYS 0.5 * number of slots -
-        // it does NOT (directly) depend on the number of wildcard-matches! This fact is very important!
-        // This algorithm ensures that wildcard-matches can only appear in the first round of the cup.
-        // There will never be wildcard-matches in other rounds than the first one!
-        $matches = [];
-        for ($matchIndex = 0; $matchIndex < $cup->slots / 2; $matchIndex++) {
-            // Note: We know that we have at least 0.5 * slots participants.
-            // This is because we have reduced the number of slots to the lowest possible value.
-            // (The difference between two slot limits is always 50% of the bigger limit.)
-            $leftParticipantId = $participants[$matchIndex]->id;
-
-            // Note: Wildcard-matches will always have set right_participant_id to 0.
-            $index = $cup->slots / 2 + $matchIndex;
-            $rightParticipantId = isset($participants[$index]) ? $participants[$index]->id : 0;
-
-            $now = new Carbon;
-            
-            $matches[] = [
-                'round'                 => 1,
-                'row'                   => $matchIndex + 1,
-                'with_teams'            => $cup->forTeams(),
-                'left_participant_id'   => $leftParticipantId,
-                'right_participant_id'  => $rightParticipantId,
-                'winner_id'             => $rightParticipantId > 0 ? 0 : $leftParticipantId,
-                'cup_id'                => $cupId,
-                'creator_id'            => user()->id,
-                'created_at'            => $now,
-            ];
-        }
-
-        Match::insert($matches);
+        $cup->seed();
 
         $this->alertFlash(trans('app.created', [trans('app.object_matches')]));
         return Redirect::to('admin/cups');
