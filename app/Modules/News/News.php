2c57ba0d30f72a466fb9c069f8e56381131ba0db
Chris Konnertz <siconize@gmail.com>
2019-07-10 16:45:42 +0200

    Use object, not facade

--- app/Modules/News/News.php
+++ app/Modules/News/News.php
@@ -9,7 +9,7 @@ use ContentFilter;
 use DB;
 use Illuminate\Database\Eloquent\Builder;
 use Lang;
-use OpenGraph;
+use ChristophKonnertz\OpenGraph\OpenGraph;
 use Rss;
 use SoftDeletingTrait;
 use URL;
