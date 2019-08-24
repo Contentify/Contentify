9af5eb17888bde8a9c751181167ead7d51cd6af0
GamePals <git@gamepals.net>
2019-08-24 15:31:57 -0300

    Portuguese Translation
    Portuguese translation

---
+++ resources/lang/pt/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'As senhas devem corresponder e conter pelo menos 6 caracteres',
+    'reset'    => 'Sua senha foi redefinida!',
+    'sent'     => 'Enviamos um e-mail com o link para redefinir sua senha!!',
+    'token'    => 'O token de recuperação de senha é inválido.',
+    'user'     => 'Não conseguimos encontrar nenhum usuário com esse email.',
+];
