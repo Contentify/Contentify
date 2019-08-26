5264cb9329eef8269b393483563fa0084afcadd4
GamePals <git@gamepals.net>
2019-08-25 21:37:21 -0300

    Translations for some modules
    Auth, Comments, Config, Contact and Cups modules has been translated into portuguese.
    
    Fixed line 23 on main.php (Spanish - Cups module)

---
+++ app/Modules/Config/Resources/Lang/pt/main.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Module Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | This file keeps the language lines of the related module.
+    |
+    */
+
+    'permissions'               => 'Permissões',
+
+    'button_info'               => 'Informação do PHP',
+    'button_optimize'           => 'Otimizar DB',
+    'button_dump'               => 'Exportar DB',
+    'button_log'                => 'Log',
+    'button_compile_less'       => 'Compilar LESS',
+    'button_clear_cache'        => 'Limpar cache',
+
+    'db_export'                 => 'Exportação de banco de dados bem-sucedida.',
+    'db_file'                   => 'Um arquivo .sql foi criado em:<br>:0',
+    'not_supported'             => 'Sentimos muito, :0 este recurso não é suportado.',
+
+    'log_empty'                 => 'O arquivo de log está vazio.',
+
+    /*
+     * Config
+     */
+    'website_name'              => 'Nome da web',
+    'analytics'                 => 'Código HTML do Analytics',
+    'registration'              => 'Registro desbloqueado',
+    'db_backup'                 => 'Cópia automática de banco de dados',
+    'forbidden_email_domains'   => 'Domínios de email bloqueados',
+    'short_biography'           => 'Biografia curta',
+
+    /*
+     * Meta information
+     */
+    'meta_author'               => 'Escritor',
+    'meta_author_info'          => 'De quem é este site?',
+    'meta_keywords'             => 'Slogans',
+    'meta_keywords_info'        => 'Otimização de sites para mecanismos de busca (slogans)',
+    'meta_description'          => 'Descrição',
+    'meta_description_info'     => 'Otimização de sites para mecanismos de busca (Descrição)',
+];
---
+++ app/Modules/Cups/Resources/Lang/pt/main.php
@@ -0,0 +1,58 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Module Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | This file keeps the language lines of the related module.
+    |
+    */
+
+    // Cup
+    'join_at'            => 'Juntar-se a',
+    'check_in_at'        => 'Registre-se em',
+    'start_at'           => 'Iniciar em',
+    'checked_in'         => 'Registrado em',
+    'seeding'            => 'Classificação',
+    'players_per_team'   => 'Jogadores por time',
+    'bracket'            => 'Bracket',
+    'cup_full'           => 'Número máximo de participantes atingidos.',
+    'cannot_join'        => 'As inscrições para o torneio ainda não foram abertas - aguarde o início da fase de inscrição.',
+    'joined'             => 'Você já está participando. Aguarde o início da fase de registro.',
+    'join_hint'          => 'Clique aqui para participar do torneio:',
+    'join'               => 'Inscreva-se agora',
+    'no_team'            => 'Você não tem aqui para participar do torneio!',
+    'create_team'        => 'Crie uma equipe',
+    'check_out'          => 'Clique aqui para sair:',
+    'check_in'           => 'Clique aqui para entrar:',
+    'not_participating'  => 'Você não está participando deste torneio.',
+    'cup_running'        => 'O torneio já começou. Por favor, olhe os brackets para ver a classificação.',
+    'cup_closed'         => 'O torneio já fechou.',
+    'login_hint'         => 'Entre para acessar este torneio!',
+    'referees'           => 'Árbitros',
+    'swap'               => 'Alterar',
+    'in_no_cups'         => 'Você não está registrado em nenhum torneio.',
+    'join_a_cup'         => 'Participe do torneio agora!',
+
+    // Team
+    'not_organizer'      => 'Somente o organizador da sua equipe pode fazer isso.',
+    'organizer'          => 'Organizador',
+    'user_conflict'      => 'Um usuário não pode estar em mais de um time por torneio.',
+    'team_locked'        => 'O time trava durante um torneio. Você não tem permissão para fazer alterações.',
+    'edit_team'          => 'Edite a equipe',
+    'team_deleted'       => 'Esta equipe foi eliminada.',
+    'my_teams'           => 'Minhas equipes',
+    'wrong_password'     => '¡senha incorreta! Por favor tente novamente.',
+    'min_organizers'     => 'Isso não é possível. A equipe deve ter pelo menos um organizador.',
+    'not_enough_players' => 'poucos jogadores',
+
+    // Match
+    'confirm_score'      => 'Confirmar resultados',
+    'new_match'          => 'Um novo encontro foi gerado!',
+    'change_winner'      => 'Mude o vencedor',
+    'next_match'         => 'Próxima partida',
+
+];
