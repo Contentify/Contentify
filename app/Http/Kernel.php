78277cfe37f3d09b208fc82bb02f2e28dd50e604
chriskonnertz <siconize@gmail.com>
2021-08-01 16:13:19 +0200

    Removed incorrect comment

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -53,7 +53,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth'          => \App\Http\Middleware\Authenticate::class, // Deactivate... we have our own implementation
+        'auth'          => \App\Http\Middleware\Authenticate::class,
         'bindings'      => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'guest'         => \App\Http\Middleware\RedirectIfAuthenticated::class,
